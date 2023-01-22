8acf5e842fb6a40745175ef05c5f3f01b03ce663
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-22 23:58:05 +0630

    Using if statement to check if that is the random number has a larger value

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,6 +1,7 @@
 def bubble_sort(random_num):
     for i in range(len(random_num)-1, 0, -1):
         for j in range(i):
+            if random_num[j] > random_num[j+1]:
 
 
 random_num = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
