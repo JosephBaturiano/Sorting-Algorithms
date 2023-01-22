9532f5e14ad9e90a99e30674e8f6e78778498a97
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-23 03:21:34 +0630

    Defining the parition function to use in quick_sort function

--- Quick_sort.py
+++ Quick_sort.py
@@ -5,4 +5,23 @@ def swap(a, b, arr):
         arr[b] = tmp
 
 
+def partition(elements, start, end):
+    pivot_index = start
+    pivot = elements[pivot_index]
+
+    while start < end:
+        while start < len(elements) and elements[start] <= pivot:
+            start += 1
+
+        while elements[end] > pivot:
+            end -= 1
+
+        if start < end:
+            swap(start, end, elements)
+
+    swap(pivot_index, end, elements)
+
+    return end
+
+
 random_num = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
