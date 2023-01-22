a2b8e0bcec1bd887c85fbeb374cd5d4d70ad47ca
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-23 01:16:11 +0630

    Create an inner loop and a condition that can process all the way to 0

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -2,6 +2,7 @@ def insertion_sort(random_num):
     for i in range(1, len(random_num)):
         sorted_area = random_num[i]
         j = i - 1
+        while j >= 0 and sorted_area < random_num[j]:
 
 
 random_num = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
