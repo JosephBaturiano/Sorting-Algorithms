69767bc59e1c2d0ea133699a7f119cfa22346340
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-23 01:20:28 +0630

    Will now do the swapping phase

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -3,6 +3,9 @@ def insertion_sort(random_num):
         sorted_area = random_num[i]
         j = i - 1
         while j >= 0 and sorted_area < random_num[j]:
+            random_num[j+1] = random_num[j]
+            j = j - 1
+        random_num[j+1] = sorted_area
 
 
 random_num = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
