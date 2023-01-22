a7b1f261d40632bb9eac69de1c6f1cee157b225a
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-23 01:13:36 +0630

    left side element which is one place less than the current one

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,6 +1,7 @@
 def insertion_sort(random_num):
     for i in range(1, len(random_num)):
         sorted_area = random_num[i]
+        j = i - 1
 
 
 random_num = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
