d613bd928906067072d777141407a2ffabdc010d
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-23 01:59:44 +0630

    Doing the merge sort in left and right array

--- Merge_sort.py
+++ Merge_sort.py
@@ -3,5 +3,8 @@ def merge_sort(arr):
         left_arr = arr[:len(arr)//2]
         right_arr = arr[len(arr)//2:]
 
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
 
 random_num = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
