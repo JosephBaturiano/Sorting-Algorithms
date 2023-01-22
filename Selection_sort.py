b68e13a698059ba8916938c1a17ae42c66354b3b
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-22 23:27:01 +0630

    Checking if the num is less than to min pos then make it as a min pos if it is true

--- Selection_sort.py
+++ Selection_sort.py
@@ -4,6 +4,8 @@ def sort(nums):
     for i in range(9):
         minpos = i
         for j in range(i, 10):
+            if nums[j] < nums[minpos]:
+                minpos = j
 
 
 nums = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
