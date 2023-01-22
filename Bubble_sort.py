8f373b571ffb1b70a1f56bcb4f07230e2f4f991b
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-23 00:02:30 +0630

    Swapping the first and the second value after claiming it's true and print it every swapping

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -2,6 +2,10 @@ def bubble_sort(random_num):
     for i in range(len(random_num)-1, 0, -1):
         for j in range(i):
             if random_num[j] > random_num[j+1]:
+                swap = random_num[j]
+                random_num[j] = random_num[j+1]
+                random_num[j+1] = swap
+                print(random_num)
 
 
 random_num = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
