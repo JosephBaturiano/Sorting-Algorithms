3e3ab2f8395bf6243dc385bba1e04aee67ea40f7
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-23 19:36:11 +0630

    Defining a function called partition

--- Quick_sort.py
+++ Quick_sort.py
@@ -1,1 +1,22 @@
+
+def partition(random_num, left, right):
+    i = left
+    j = right - 1
+    pivot = random_num[right]
+
+    while i < j:
+        while i < right and random_num[i] < pivot:
+            i += 1
+        while j > left and random_num[j] >= pivot:
+            j -= 1
+
+        if i < j:
+            random_num[i], random_num[j] = random_num[j], random_num[i]
+
+    if random_num[i] > pivot:
+        random_num[i], random_num[right] = random_num[right], random_num[i]
+    print("\t\t\t ", random_num)
+    return i
+
+
 random_num = [66, 35, 88, 93, 28, 59, 97, 69, 62, 9]
