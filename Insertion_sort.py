1de11ecd72a405fbf04914ba0edf314a43dc1a43
Joseph Z Baturiano <josephbaturiano3@gmail.com>
2023-01-23 14:56:20 +0630

    Renaming some variable

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,11 +1,11 @@
 def insertion_sort(random_num):
     for i in range(1, len(random_num)):
-        sorted_area = random_num[i]
+        anchor = random_num[i]
         j = i - 1
-        while j >= 0 and sorted_area < random_num[j]:
+        while j >= 0 and anchor < random_num[j]:
             random_num[j+1] = random_num[j]
             j = j - 1
-        random_num[j+1] = sorted_area
+        random_num[j+1] = anchor
         print(random_num)
 
 
